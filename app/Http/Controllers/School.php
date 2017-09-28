fd0e1df24acc7c6a0dc5f2e153a5548bd0f55c84
Mark Miller <constenum@gmail.com>
2017-09-27 20:59:17 -0400

    fix controllers

--- app/Http/Controllers/School.php
+++ app/Http/Controllers/School.php
@@ -3,10 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Student;
 use App\School;
 
-class StudentController extends Controller
+class SchoolController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -15,13 +14,7 @@ class StudentController extends Controller
      */
     public function index()
     {
-        # get all schools
-        $schools = School::all();
-
-        # get all students
-        $students = Student::all();
-
-        return view('student.index')->with(['schools' => $schools, 'students' => $students]);
+        //
     }
 
     /**
@@ -31,9 +24,7 @@ class StudentController extends Controller
      */
     public function create()
     {
-        # get all schools
-        $schools = School::all();
-        return view('student/create')->with(['schools' => $schools]);
+        //
     }
 
     /**
@@ -44,43 +35,16 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        # Validate New Student Form
-        $this->validate($request, [
-            'school_id' => 'required',
-            'first_name' => 'required',
-            'last_name' => 'required',
-        ]);
-
-        # Insert New Student Record
-        $student = new Student();
-        $student->school_id = $request->input('school_id');
-        $student->first_name = $request->input('first_name');
-        $student->last_name = $request->input('last_name');
-        $student->preferred_name = $request->input('preferred_name');
-        $student->street_address = $request->input('street_address');
-        $student->city = $request->input('city');
-        $student->state = $request->input('state');
-        $student->zip_code = $request->input('zip_code');
-        $student->telephone_number = $request->input('telephone_number');
-        $student->phone_type = $request->input('phone_type');
-        $student->mother_first_name = $request->input('mother_first_name');
-        $student->mother_last_name = $request->input('mother_last_name');
-        $student->father_first_name = $request->input('father_first_name');
-        $student->father_last_name = $request->input('father_last_name');
-        $student->save();
-
-        \Session::flash('flash_message', 'Thank you, you have checked in successfully!');
-
-        return redirect('/student');
+        //
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Student  $student
+     * @param  \App\School  $school
      * @return \Illuminate\Http\Response
      */
-    public function show(Student $student)
+    public function show(School $school)
     {
         //
     }
@@ -88,64 +52,33 @@ class StudentController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
+     * @param  \App\School  $school
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(School $school)
     {
-        $student = Student::find($id);
-
-        # get all schools
-        $schools = School::all();
-
-        return view('student.edit')->with(['schools' => $schools, 'student' => $student]);
+        //
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Student  $student
+     * @param  \App\School  $school
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Student $student)
+    public function update(Request $request, School $school)
     {
-        # Validate Edit Student Form
-        $this->validate($request, [
-            'school_id' => 'required',
-            'first_name' => 'required',
-            'last_name' => 'required',
-        ]);
-
-        # Update Student Record
-        $student = Student::find($request->id);
-        $student->school_id = $request->input('school_id');
-        $student->first_name = $request->input('first_name');
-        $student->last_name = $request->input('last_name');
-        $student->preferred_name = $request->input('preferred_name');
-        $student->street_address = $request->input('street_address');
-        $student->city = $request->input('city');
-        $student->state = $request->input('state');
-        $student->zip_code = $request->input('zip_code');
-        $student->telephone_number = $request->input('telephone_number');
-        $student->phone_type = $request->input('phone_type');
-        $student->mother_first_name = $request->input('mother_first_name');
-        $student->mother_last_name = $request->input('mother_last_name');
-        $student->father_first_name = $request->input('father_first_name');
-        $student->father_last_name = $request->input('father_last_name');
-        $student->save();
-
-        \Session::flash('flash_message', 'Thank you, you have checked in successfully!');
-
-        return redirect('/student');
+        //
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Student  $student
+     * @param  \App\School  $school
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Student $student)
+    public function destroy(School $school)
     {
         //
     }
--- app/Http/Controllers/Student.php
+++ app/Http/Controllers/Student.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Student;
+use App\School;
 
-class Student extends Controller
+class StudentController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +15,13 @@ class Student extends Controller
      */
     public function index()
     {
-        //
+        # get all schools
+        $schools = School::all();
+
+        # get all students
+        $students = Student::all();
+
+        return view('student.index')->with(['schools' => $schools, 'students' => $students]);
     }
 
     /**
@@ -23,7 +31,9 @@ class Student extends Controller
      */
     public function create()
     {
-        //
+        # get all schools
+        $schools = School::all();
+        return view('student/create')->with(['schools' => $schools]);
     }
 
     /**
@@ -34,16 +44,43 @@ class Student extends Controller
      */
     public function store(Request $request)
     {
-        //
+        # Validate New Student Form
+        $this->validate($request, [
+            'school_id' => 'required',
+            'first_name' => 'required',
+            'last_name' => 'required',
+        ]);
+
+        # Insert New Student Record
+        $student = new Student();
+        $student->school_id = $request->input('school_id');
+        $student->first_name = $request->input('first_name');
+        $student->last_name = $request->input('last_name');
+        $student->preferred_name = $request->input('preferred_name');
+        $student->street_address = $request->input('street_address');
+        $student->city = $request->input('city');
+        $student->state = $request->input('state');
+        $student->zip_code = $request->input('zip_code');
+        $student->telephone_number = $request->input('telephone_number');
+        $student->phone_type = $request->input('phone_type');
+        $student->mother_first_name = $request->input('mother_first_name');
+        $student->mother_last_name = $request->input('mother_last_name');
+        $student->father_first_name = $request->input('father_first_name');
+        $student->father_last_name = $request->input('father_last_name');
+        $student->save();
+
+        \Session::flash('flash_message', 'Thank you, you have checked in successfully!');
+
+        return redirect('/student');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Student  $student
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Student $student)
     {
         //
     }
@@ -51,33 +88,64 @@ class Student extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        //
+        $student = Student::find($id);
+
+        # get all schools
+        $schools = School::all();
+
+        return view('student.edit')->with(['schools' => $schools, 'student' => $student]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Student  $student
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Student $student)
     {
-        //
+        # Validate Edit Student Form
+        $this->validate($request, [
+            'school_id' => 'required',
+            'first_name' => 'required',
+            'last_name' => 'required',
+        ]);
+
+        # Update Student Record
+        $student = Student::find($request->id);
+        $student->school_id = $request->input('school_id');
+        $student->first_name = $request->input('first_name');
+        $student->last_name = $request->input('last_name');
+        $student->preferred_name = $request->input('preferred_name');
+        $student->street_address = $request->input('street_address');
+        $student->city = $request->input('city');
+        $student->state = $request->input('state');
+        $student->zip_code = $request->input('zip_code');
+        $student->telephone_number = $request->input('telephone_number');
+        $student->phone_type = $request->input('phone_type');
+        $student->mother_first_name = $request->input('mother_first_name');
+        $student->mother_last_name = $request->input('mother_last_name');
+        $student->father_first_name = $request->input('father_first_name');
+        $student->father_last_name = $request->input('father_last_name');
+        $student->save();
+
+        \Session::flash('flash_message', 'Thank you, you have checked in successfully!');
+
+        return redirect('/student');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Student  $student
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Student $student)
     {
         //
     }
